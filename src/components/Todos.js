fde94315b17f53f6bd9530988e0870710775e1e9
Rizky Puji Raharja <rizky.rsj@gmail.com>
2020-09-27 15:35:43 +0700

    Remove unused import

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import ls from 'local-storage'
 import TodoItem from './TodoItem'
 import { removeTodo, markDoneTodo } from './../store'
 
